f4423687a9a1737d77e682bed79f31039729ee61
Amogelang-JJ <amogelangmkse@gmail.com>
2025-01-11 21:43:43 +0200

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <Link to="/">About</Link>
         <Link to="/projects">Projects</Link>
-        <Link to="/articles">Q&A</Link>
+        <Link to="/articles">Business</Link>
         <Link to="/ContactForm">Contacts</Link>
       </div>
       <div className="menu-icon" onClick={toggleNavbar}>
